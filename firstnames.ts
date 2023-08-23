6359fd366715ce042e0f1111825330ddb67440e3
thinkier <26728734+thinkier@users.noreply.github.com>
2023-08-23 10:32:21 +1000

    reorg

---
+++ firstnames.ts
@@ -0,0 +1,8 @@
+import fetch from "node-fetch";
+
+fetch("https://checkip.amazonaws.com")
+    .then(res => res.text())
+    .then(body => {
+        console.log(`${body}`);
+    });
+
--- index.ts
+++
@@ -1,7 +0,0 @@
-import fetch from "node-fetch";
-
-fetch("https://checkip.amazonaws.com")
-    .then(res => res.text())
-    .then(body => {
-        console.log(`${body}`);
-    });
